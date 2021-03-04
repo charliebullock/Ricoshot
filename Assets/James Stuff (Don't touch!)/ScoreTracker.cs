5f683ba32f10812ddca9c2297bd3b51b9cfbbd7f
James Bradbury <unkoun98@gmail.com>
2021-03-04 23:08:41 +0000

    charlies models

---
+++ Assets/James Stuff (Don't touch!)/ScoreTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour
+{
+    [Serializable]
+    public class UICounters
+    {
+        public string name;
+        public int counter;
+        public Text counterDisplay;
+    }
+    public UICounters[] MenuCounters;
+}
