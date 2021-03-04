23392cfd70b3e3b3b35d38c5b08baa7cf7e60c78
James Bradbury <unkoun98@gmail.com>
2021-03-04 20:52:30 +0000

    James Push
    shaders, sound manager, menu singleton

---
+++ Assets/James Stuff (Don't touch!)/James Scripts/MenuSingleton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSingleton : MonoBehaviour
+{
+
+    
+    public static MenuSingleton instance;
+
+    
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+    public void ChangeLevel(int level)
+    {
+
+        SceneManager.LoadScene(level);
+    
+    }
+
+    public void PlaySound(AudioClip PlayMe, Vector3 AtHere, float PitchVariation)
+    {
+        GameObject MyObject = new GameObject(PlayMe.name);
+
+        MyObject.transform.position = AtHere;
+        AudioSource MyAudio = MyObject.AddComponent<AudioSource>();
+
+        MyAudio.pitch = MyAudio.pitch + Random.Range(-PitchVariation, PitchVariation);
+        MyAudio.clip = PlayMe;
+        MyAudio.Play();
+
+        Destroy(MyObject, PlayMe.length);
+    }
+
+
+}
---
+++ Assets/James Stuff (Don't touch!)/James Scripts/PlaySound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void CreateSFX(AudioClip audioClip)
+    {
+        MenuSingleton menuSingleton = MenuSingleton.instance;
+
+        menuSingleton.PlaySound(audioClip, transform.position, 0);
+    }
+}
